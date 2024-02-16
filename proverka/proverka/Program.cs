996d49ad0e99732f7f82e2374c658ed54c23b6b1
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-02-16 20:13:26 +0200

    ubdeit

---
+++ proverka/proverka/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Въведете възрастта си:");
+int age = int.Parse(Console.ReadLine());
+
+if (age > 21)
+{
+    Console.WriteLine("Пълнолетен!");
+}
+else
+{
+    Console.WriteLine("Непълнолетен!");
+}
