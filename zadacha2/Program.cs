b194a8f50f0a9351b80872f0d83639a6e879e672
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-03-22 10:23:00 +0200

    zadacha2 masivi

---
+++ zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums1 = { 1, 2, 3, 4, 5, 6, 7, 9 };
+int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int sum = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    sum += nums[i];
+    if (sum == nums1[i])
+    {
+        Console.WriteLine("Да!");
+    }
+    else
+    {
+        Console.WriteLine("Не!");
+
+    }
+}
